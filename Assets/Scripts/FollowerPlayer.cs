3cc02032e546f9e954ef98bf3e59175b8230370b
Shota <Shota@SHOTA-THINK>
2016-08-18 19:46:46 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Scripts/FollowerPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowerPlayer : MonoBehaviour {
+    public Transform target;
+    private Vector3 offset;
+    
+    void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Transform>().position = target.position + offset;
+	}
+}
